b8756ff2db4bf689371245e2c403017894feef16
Tony Narlock <tony@git-pull.com>
2021-09-29 23:13:05 -0500

    Run prettier with new config

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,37 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 type DeploymentState =
-  | "error"
-  | "failure"
-  | "inactive"
-  | "in_progress"
-  | "queued"
-  | "pending"
-  | "success";
+  | 'error'
+  | 'failure'
+  | 'inactive'
+  | 'in_progress'
+  | 'queued'
+  | 'pending'
+  | 'success'
 
 async function run() {
   try {
-    const context = github.context;
-    const logUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
+    const context = github.context
+    const logUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`
 
-    const token = core.getInput("token", { required: true });
-    const ref = core.getInput("ref", { required: false }) || context.ref;
-    const url = core.getInput("target_url", { required: false }) || logUrl;
+    const token = core.getInput('token', {required: true})
+    const ref = core.getInput('ref', {required: false}) || context.ref
+    const url = core.getInput('target_url', {required: false}) || logUrl
     const environment =
-      core.getInput("environment", { required: false }) || "production";
-    const description = core.getInput("description", { required: false });
+      core.getInput('environment', {required: false}) || 'production'
+    const description = core.getInput('description', {required: false})
     const initialStatus =
-      (core.getInput("initial_status", {
+      (core.getInput('initial_status', {
         required: false
-      }) as DeploymentState) || "pending";
-    const autoMergeStringInput = core.getInput("auto_merge", {
+      }) as DeploymentState) || 'pending'
+    const autoMergeStringInput = core.getInput('auto_merge', {
       required: false
-    });
+    })
 
-    const auto_merge: boolean = autoMergeStringInput === "true";
+    const auto_merge: boolean = autoMergeStringInput === 'true'
 
-    const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
+    const client = new github.GitHub(token, {previews: ['flash', 'ant-man']})
 
     const deployment = await client.repos.createDeployment({
       owner: context.repo.owner,
@@ -42,7 +42,7 @@ async function run() {
       transient_environment: true,
       auto_merge,
       description
-    });
+    })
 
     await client.repos.createDeploymentStatus({
       ...context.repo,
@@ -50,13 +50,13 @@ async function run() {
       state: initialStatus,
       log_url: logUrl,
       environment_url: url
-    });
+    })
 
-    core.setOutput("deployment_id", deployment.data.id.toString());
+    core.setOutput('deployment_id', deployment.data.id.toString())
   } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
+    core.error(error)
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
